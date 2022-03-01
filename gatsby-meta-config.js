4f76b9a6af991067606a5b6271527b7f4f4b7a91
inticoy <inticoy0406@gmail.com>
2022-03-02 01:52:30 +0900

    2022.03.02. AM 01:52

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `Inticoy`,
+  description: `Inticoy's Blog`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://inticoy.github.io`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
@@ -11,16 +11,16 @@ module.exports = {
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `윤건우`,
     bio: {
       role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
+      description: ['인생이 즐거운', '다재다능한', '본질을 꿰뚫고 싶은'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
+      github: `https://github.com/inticoy`, // `https://github.com/zoomKoding`,
       linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      email: `inticoy0406@gmail.com`, // `zoomkoding@gmail.com`,
     },
   },
 
@@ -43,12 +43,28 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2021.02 ~',
-        activity: '개인 블로그 개발 및 운영',
+        date: '2022.02',
+        activity: '한양대학교 EOS 학술제 Advanced 우승',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          github: 'https://github.com/penta-trespassers',
+          demo: 'https://www.figma.com/proto/wlmYpiATcrL3YwPf5Hy342/Neodoallae-Prototype?node-id=2%3A5&starting-point-node-id=2%3A5&show-proto-sidebar=1',
+        },
+      },
+      {
+        date: '2021.08 ~ 2022.02',
+        activity: 'NC Fellowship Program Neural Graphics Track 참가',
+        links: {
+          //github: 'https://github.com/penta-trespassers',
+          //demo: 'https://www.figma.com/proto/wlmYpiATcrL3YwPf5Hy342/Neodoallae-Prototype?node-id=2%3A5&starting-point-node-id=2%3A5&show-proto-sidebar=1',
+        },
+      },
+      {
+        date: '2021.05 ~',
+        activity: '블로그 시작',
+        links: {
+          //post: '/gatsby-starter-zoomkoding-introduction',
+          github: 'https://github.com/inticoy/inticoy.github.io',
+          demo: 'https://inticoy.github.io',
         },
       },
     ],
@@ -71,18 +87,30 @@ module.exports = {
       },
       // ========================================================
       // ========================================================
+
       {
-        title: '개발 블로그 테마 개발',
-        description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
+        title: '너도 올래? 앱 디자인 및 개발',
+        description: '친구의 자취방을 숙소처럼 예약할 수 있는 앱이 있으면 좋겠다고 생각하여 주요 기능으로 친구, 예약, 내 주변 자취방 검색 기능을 구현하였습니다. Penta-Trespassers 팀원들과 함께 교내 앱 개발 동아리 EOS 학술제에서 우승하는 성과를 이루어냈습니다. 2022년에는 서버의 Django에서 Ktor로의 이주와 플레이스토어 출시를 계획하고 있습니다.',
+        techStack: ['figma', 'kotlin', 'android'],
+        thumbnailUrl: 'neodoollae.png',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          github: 'https://github.com/penta-trespassers',
+          demo: 'https://www.figma.com/proto/wlmYpiATcrL3YwPf5Hy342/Neodoallae-Prototype?node-id=2%3A5&starting-point-node-id=2%3A5&show-proto-sidebar=1',
         },
       },
+
+      // {
+      //   title: '개발 블로그 테마 개발',
+      //   description:
+      //     '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
+      //   techStack: ['gatsby', 'react'],
+      //   thumbnailUrl: 'blog.png',
+      //   links: {
+      //     post: '/gatsby-starter-zoomkoding-introduction',
+      //     github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+      //     demo: 'https://www.zoomkoding.com',
+      //   },
+      // },
     ],
   },
 };
