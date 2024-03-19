f03c7a1b2fbbef2f5efba2840be35255b93671a8
gyoon <inticoy0406@gmail.com>
2024-03-19 13:45:05 +0900

    feat: apply noto sans

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("content").innerHTML = marked(
+  "# Marked in browser\n\nRendered by **marked**." +
+    "\n\n![hi](asset/logo/logo_inticoy_darkmode.svg)\n\n```c\nint a = b;```\n\n`a` is a "
+);
