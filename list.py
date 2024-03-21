e88d4c79f0a36661f7e4b89c261ccbe17331baef
inticoy <inticoy0406@gmail.com>
2024-03-21 17:39:21 +0900

    feat: list test

---
+++ list.py
@@ -0,0 +1,32 @@
+import os
+import json
+
+def list_files_in_directories(base_path):
+    directory_structure = {}
+    categories = []
+    
+    for item in os.listdir(base_path):
+        categories.append([item, os.path.join(base_path, item)])
+    
+    categories.sort()
+    print(categories)
+    
+    for category, category_path in categories:
+        if os.path.isdir(category_path):
+            files = [{"title": f, "date": f[:10], "category":category, "path":os.path.join(category_path, f)} for f in os.listdir(category_path) if os.path.isfile(os.path.join(category_path, f))]
+            # files.sort()
+            directory_structure[category] = files
+    
+    return directory_structure
+
+def save_structure_as_json(structure, output_file):
+    with open(output_file, 'w') as f:
+        json.dump(structure, f, indent=4)
+
+base_path = './contents'
+
+output_file = './contents/contents.json'
+
+directory_structure = list_files_in_directories(base_path)
+
+save_structure_as_json(directory_structure, output_file)
--- posts/script.js
+++ posts/script.js
@@ -1,4 +1,55 @@
-document.getElementById("content").innerHTML = marked(
-  "# Marked in browser\n\nRendered by **marked**." +
-    "\n\n![hi](asset/logo/logo_inticoy_darkmode.svg)\n\n```c\nint a = b;```\n\n`a` is a "
-);
+var dirStructure;
+var categories = [];
+var posts = [];
+
+async function loadDirStructure() {
+  try {
+    const response = await fetch("/contents/contents.json");
+    if (!response.ok) {
+      throw new Error("fetching file list failed");
+    }
+    dirStructure = await response.json();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function loadCategories() {
+  for (var key in dirStructure) {
+    categories.push(key);
+  }
+}
+
+function loadPosts() {
+  for (var key in dirStructure) {
+    posts = posts.concat(dirStructure[key]);
+  }
+}
+
+function sortPosts() {
+  posts.sort(function (a, b) {
+    if (a.date < b.date) return -1;
+    if (a.date > b.date) return 1;
+    return a.title.localeCompare(b.title);
+  });
+}
+
+function showPosts() {
+  document.getElementById("content").innerHTML = "";
+  for (const e of posts) {
+    var item = document.createElement("div");
+    item.innerText = e["title"] + " " + e["date"] + " " + e["category"];
+    document.getElementById("content").appendChild(item);
+  }
+}
+
+loadDirStructure()
+  .then(loadCategories)
+  .then(loadPosts)
+  .then(sortPosts)
+  .then(showPosts);
+
+// document.getElementById("content").innerHTML = marked(
+//   "# Marked in browser\n\nRendered by **marked**." +
+//     "\n\n![hi](asset/logo/logo_inticoy_darkmode.svg)\n\n```c\nint a = b;```\n\n`a` is a "
+// );
