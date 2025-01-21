d2940052bde07fd15adbdf4d80015c0ff9c0171b
Geonwoo <55380241+inticoy@users.noreply.github.com>
2025-01-21 23:17:04 +0900

    Feat: index.js (#4)
    * feat: darkmode
    
    * feat: run index

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ RUN apt-get update && \
     apt-get upgrade && \
     apt-get install -y python3 
 
-COPY . /usr/share/nginx/html
+COPY build /usr/share/nginx/html
 COPY ./tools/default.conf /etc/nginx/conf.d/default.conf
 
 WORKDIR /usr/share/nginx/html/
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import fs from "fs";
+import { promises as fsPromises } from "fs";
 import matter from "gray-matter";
 import path from "path";
 import { glob } from "glob";
@@ -13,10 +14,17 @@ const readFile = (filename) => {
   return { ...parsed, html };
 };
 
-const templatize = (template, { date, title, content }) =>
+const templatize = (
+  template,
+  { categories, title, summary, date, tags, thumbnail, content }
+) =>
   template
-    .replace(/<!-- PUBLISH_DATE -->/g, date)
+    .replace(/<!-- CATEGORY -->/g, categories)
     .replace(/<!-- TITLE -->/g, title)
+    .replace(/<!-- SUMMARY -->/g, summary)
+    .replace(/<!-- PUBLISH_DATE -->/g, date)
+    // .replace(/<!-- TAG -->/g, tags)
+    // .replace(/<!-- THUMBNAIL -->/g, thumbnail)
     .replace(/<!-- CONTENT -->/g, content);
 
 const saveFile = (filename, contents) => {
@@ -27,7 +35,7 @@ const saveFile = (filename, contents) => {
 
 const getOutputFilename = (filename, outPath) => {
   const basename = path.basename(filename);
-  const newfilename = basename.substring(0, basename.length - 3) + ".html";
+  const newfilename = basename.substring(11, basename.length - 3) + ".html";
   const outfile = path.join(outPath, newfilename);
   return outfile;
 };
@@ -37,8 +45,12 @@ const processFile = (filename, template, outPath) => {
   const outfilename = getOutputFilename(filename, outPath);
 
   const templatized = templatize(template, {
-    date: file.data.date,
+    categories: file.data.categories,
     title: file.data.title,
+    summary: file.data.summary,
+    date: file.data.date,
+    tags: file.data.tags,
+    thumbnail: file.data.thumbnail,
     content: file.html,
   });
 
@@ -46,22 +58,54 @@ const processFile = (filename, template, outPath) => {
   console.log(`📝 ${outfilename}`);
 };
 
-const main = () => {
-  const contentPath = path.resolve("content");
-  const postPath = path.join(contentPath, "posts");
+async function copyStaticFiles(files) {
+  try {
+    await Promise.all(
+      files.map(async ({ source, destination }) => {
+        // 복사할 디렉터리 존재 여부 확인 후 생성
+        const dir = path.dirname(destination);
+        mkdirp.sync(dir);
 
+        // 파일 복사
+        await fsPromises.copyFile(source, destination);
+        console.log(`복사 성공: ${source} → ${destination}`);
+      })
+    );
+    console.log("모든 파일 복사가 완료되었습니다!");
+  } catch (err) {
+    console.error("파일 복사 중 오류 발생:", err);
+  }
+}
+
+// 사용 예시
+
+const main = () => {
+  const staticFiles = [
+    { source: "./src/tailwind.css", destination: "./build/src/tailwind.css" },
+    { source: "./src/app.js", destination: "./build/src/app.js" },
+  ];
+
+  copyStaticFiles(staticFiles);
+
+  const contentPath = path.resolve("content");
+  const contentPostsPath = path.join(contentPath, "posts");
   const templatePath = path.resolve("template");
   const outPath = path.resolve("build");
-  // const template = fs.readFileSync(path.join(srcPath, "template.html"), "utf8");
+  const outPostsPath = path.join(outPath, "posts");
+
+  // read template
   const template = fs.readFileSync(
     path.join(templatePath, "template.html"),
     "utf8"
   );
 
-  const filenames = glob.sync(postPath + "/*.md");
+  // index.html
+  processFile(path.join(contentPath, "YYYY-MM-DD-index.md"), template, outPath);
 
+  // posts
+  const filenames = glob.sync(contentPostsPath + "/*.md");
   filenames.forEach((filename) => {
-    processFile(filename, template, outPath);
+    processFile(filename, template, outPostsPath);
   });
 };
 
