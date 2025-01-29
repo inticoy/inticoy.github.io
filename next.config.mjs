eb0942ec1d8b3f4f9f721b32eed3fee63381320e
inticoy <inticoy0406@gmail.com>
2025-01-29 21:33:50 +0900

    feat: gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
-  output: "export", // ✅ SSG 전용으로 변환
+  output: "export",
 };
 
 const withMDX = createMDX({
